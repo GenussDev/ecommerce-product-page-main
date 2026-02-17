667ed064e8c8bc5f47a62e704329b6b44b438d2a
Jeremy Lagier <jeremy.ljp97@gmail.com>
2026-02-17 11:45:00 -0400

    Ajout effet sur les miniatures

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
 function changeImage(element) {
   document.getElementById("mainImg").src = element.src;
+  
+  // Supprimer active de toutes les miniatures
+  let miniatures = document.querySelectorAll(".miniature");
+  miniatures.forEach(img => img.classList.remove("active"));
+
+  // Ajouter active sur celle cliquée
+  element.classList.add("active");
 }
+
+
